8f701825109f6099ae196d8fc808852d5e2a3ef2
Benjamin Pearce <hawk.git@bcpearce.com>
2014-09-03 20:34:53 -0400

    added handling for failed login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,14 @@ class SessionsController < ApplicationController
 	end
 	
 	def create
+		user = User.find_by(email: params[:session][:email].downcase)
+		if user && user.authenticate(params[:session][:password])
+			#sign the user in and redirect to that user's show page
+		else 
+			#create an error message and re-render the signin form
+			flash.now[:error] = 'Invalid email/password combination'
+			render 'new'
+		end
 	end
 	
 	def destroy
