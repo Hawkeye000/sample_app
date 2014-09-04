953c57e4871acf9aa26c90722e46bced0076f473
Benjamin Pearce <hawk.git@bcpearce.com>
2014-09-03 21:02:57 -0400

    added session tracking for signed in users

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,8 @@ class SessionsController < ApplicationController
 		user = User.find_by(email: params[:session][:email].downcase)
 		if user && user.authenticate(params[:session][:password])
 			#sign the user in and redirect to that user's show page
+			sign_in user
+			redirect_to user
 		else 
 			#create an error message and re-render the signin form
 			flash.now[:error] = 'Invalid email/password combination'
